7f5f2c4440b96ebfa692c8ed6e5492e142770c43
Raynger Daynger <81805282+rayswerld@users.noreply.github.com>
2022-11-23 22:16:29 -0800

    An almost functional console only game of rock, paper, scissors

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 console.log("hi Ser Ray.")
 
-const myArray = ["Rock", "Paper", "Scissors"];
+const computerSelection = ["Rock", "Paper", "Scissors"];
+const playerSelection = prompt();
 
-function computerPlay() {
-  return myArray[~~(Math.random() * myArray.length)];
+function getComputerChoice() {
+  return computerSelection[~~(Math.random() * computerSelection.length)];
 }
-console.log(computerPlay());
+console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === 'rock' && computerSelection === "Paper"){
+        return "you stinker!";
+    } else if (playerSelection === 'paper' && computerSelection === "Scissors"){
+        return 'oh lawd ray done died!';
+    } else if (playerSelection === 'scissor' && computerSelection === "Rock"){
+        return "you ice cold";
+    } else {
+        return "what the heck?!";
+    }
+}
+console.log(playRound());
