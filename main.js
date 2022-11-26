560a721d292142144524993b3caced1503712066
Raynger Daynger <81805282+rayswerld@users.noreply.github.com>
2022-11-26 15:01:22 -0800

    adding notes

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 console.log("hi Ser Ray.")
-
+//this variable is what computer is selecting from followed by player's prompt for input
 const computerSelection = ["Rock", "Paper", "Scissors"];
 const playerSelection = prompt();
 
+//this returns the comp's random selection from above array
 function getComputerChoice() {
   return computerSelection[~~(Math.random() * computerSelection.length)];
 }
 console.log(getComputerChoice());
 
+//this func kind of plays a game of paper rock scissors
 function playRound(playerSelection, computerSelection){
     if (playerSelection === 'rock' && computerSelection === "Paper"){
         return "you stinker!";
@@ -19,4 +21,14 @@ function playRound(playerSelection, computerSelection){
         return "what the heck?!";
     }
 }
+
+//here I am trying to loop the game to keep it playing it not working
+/*function game(){
+    for (let i = 0; i < 5; i++) {
+        playRound();
+
+        
+     }
+}
+console.log(game());*/
 console.log(playRound());
